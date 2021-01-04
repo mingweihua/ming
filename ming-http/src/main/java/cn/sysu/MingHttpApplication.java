400e55eba4f40fbd3d6c496aeb6f1bc3a2dd8955
mingwh <51823769+mingwh@users.noreply.github.com>
2021-01-04 17:33:22 +0800

    ming-http

---
+++ ming-http/src/main/java/cn/sysu/MingHttpApplication.java
@@ -0,0 +1,13 @@
+package cn.sysu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingHttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MingHttpApplication.class, args);
+    }
+
+}
