b2fdf5586fb07e38f5d9600e67d56f9c3e35631f
mingwh <51823769+mingwh@users.noreply.github.com>
2021-01-04 18:54:44 +0800

    2021-01-04

---
+++ ming-http/src/main/java/cn/sysu/pojo/Product.java
@@ -0,0 +1,12 @@
+package cn.sysu.pojo;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+    private Integer id;
+
+    private String name;
+    private Integer price;
+}
--- ming-http/src/test/java/cn/sysu/MingHttpApplicationTests.java
+++ ming-http/src/test/java/cn/sysu/MingHttpApplicationTests.java
@@ -1,13 +1,21 @@
 package cn.sysu;
 
+import cn.sysu.pojo.Product;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class MingHttpApplicationTests {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Test
     void contextLoads() {
+        Product product = restTemplate.getForObject("http://localhost:8080/product/2",Product.class);
+        System.out.println(product);
     }
 
 }
