b92c8428297858ab9126f7c40c62db86a119d830
mingwh <51823769+mingwh@users.noreply.github.com>
2021-01-06 19:04:40 +0800

    2021-01-06

--- ming-cloud/consumer/src/main/java/cn/sysu/controller/ConsumerController.java
+++ ming-cloud/consumer/src/main/java/cn/sysu/controller/ConsumerController.java
@@ -2,8 +2,12 @@ package cn.sysu.controller;
 
 import cn.sysu.pojo.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 
 @RestController
@@ -13,9 +17,16 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @RequestMapping("{id}")
     public Product product(@PathVariable("id") int id) {
-        String url = "http://localhost:8080/product/" + id;
+        //根据服务id获取实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("service");
+        //从实例中取ip和端口
+        ServiceInstance instance = instances.get(0);
+        String url = "http://"+ instance.getHost()+ ":" + instance.getPort()+ "/product/" + id;
         Product product = restTemplate.getForObject(url,Product.class);
         return product;
     }
